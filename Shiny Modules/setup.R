87dc233d4793423f8d5a96a7ca2cf453f3f9ae56
zannah-rain <zoe.pellatt.0@gmail.com>
2018-10-13 19:14:18 +0100

    Fix namespacing in setup module

--- Shiny Modules/setup.R
+++ Shiny Modules/setup.R
@@ -4,10 +4,10 @@ setupUI <- function(id) {
   ns <- NS(id)
   
   tabPanel("Setup",
-           checkboxInput("stringsAsFactors_checkbox", "stringsAsFactors TODO", FALSE),
-           textInput("data_source_file", "Data path: TODO"),
-           fileInput("data_source_upload", "Upload data: TODO"),
-           actionButton("source_submit_button", "Load"))
+           checkboxInput(ns("stringsAsFactors_checkbox"), "stringsAsFactors TODO", FALSE),
+           textInput(ns("data_source_file"), "Data path: TODO"),
+           fileInput(ns("data_source_upload"), "Upload data: TODO"),
+           actionButton(ns("source_submit_button"), "Load"))
 }
 
 setup <- function(input, output, session) {
