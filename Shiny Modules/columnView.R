88c8f9835b2b9e5ed6ea430dc1947535f36d2dc8
zannah-rain <zoe.pellatt.0@gmail.com>
2018-10-13 19:13:56 +0100

    Fix columnView module and migrate distPlot to it

--- Shiny Modules/columnView.R
+++ Shiny Modules/columnView.R
@@ -4,23 +4,32 @@ columnViewUI <- function(id) {
   ns <- NS(id)
   
   tabPanel("Column View",
-           # Sidebar with a slider input for number of bins 
+           # Sidebar with a slider input for number of bins
            sidebarLayout(
              sidebarPanel(
-               selectInput("column_selector",
+               selectInput(ns("column_selector"),
                            "Column to view: TODO",
                            c("DEF", "DEF"))
-             ),
-             
-             # Show a plot of the generated distribution
-             mainPanel(
-               plotOutput("distPlot"),
-               textInput("missing_fill_box", "Fill NAs with: TODO"),
-               textInput("rename_column_box", "Rename column: TODO")
-             )
+           ),
+
+           # Show a plot of the generated distribution
+           mainPanel(
+             plotOutput(ns("distPlot")),
+             textInput(ns("missing_fill_box"), "Fill NAs with: TODO"),
+             textInput(ns("rename_column_box"), "Rename column: TODO")
            ))
+  )
 }
 
 columnView <- function(input, output, session) {
+  # output$distPlot <- renderPlot({
+  #   # generate bins based on input$bins from ui.R
+  #   x    <- faithful[, 2] 
+  #   bins <- seq(min(x), max(x), length.out = 31)
+  #   
+  #   # draw the histogram with the specified number of bins
+  #   hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  # })
   
+  return(NULL)
 }
--- app.R
+++ app.R
@@ -36,15 +36,7 @@ server <- function(input, output) {
                        output_data = iris)
   
   callModule(setup, "setup_id")
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
+  callModule(columnView, "column_view_id")
 }
 
 # Run the application 
