f3efb5665e5a061398224828ba6055dfe33a3520
zannah-rain <zoe.pellatt.0@gmail.com>
2018-10-13 22:56:00 +0100

    Fix namespacing in overallSummary module

--- Shiny Modules/overallSummary.R
+++ Shiny Modules/overallSummary.R
@@ -4,11 +4,11 @@ overallSummaryUI <- function(id) {
   ns <- NS(id)
   
   tabPanel("Summary View",
-           checkboxGroupInput("summary_checkboxes", 
+           checkboxGroupInput(ns("summary_checkboxes"), 
                               "Response(s): TODO",
                               c("DEF", "DEF")),
-           tableOutput("table_metadataTODO"),
-           tableOutput("health_summaryTODO"))
+           tableOutput(ns("table_metadataTODO")),
+           tableOutput(ns("health_summaryTODO")))
 }
 
 overallSummary <- function(input, output, session) {
